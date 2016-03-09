6d2ea44ec25632425ba617b9d5e66514a9b64c9f
J. Munford <jaemunford@gmail.com>
2016-03-09 15:12:20 -0500

    finished routes and added landing page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var Bing = require('node-bing-api')({
 	accKey: "ilUTd0pcELN33xb0Sy4PHCdZaiBsqp3VdT2VLZrfBq8"
 });
 var bodyParser = require('body-parser');
+var async = require('async');
 
 var db = require("./db.js");
 var app = express();
@@ -23,66 +24,54 @@ app.post('/search/', function(req, res) {
 	res.redirect('/search/' + req.body.query);
 });
 
-app.get('/search/:query', function(req, res) {
-	query = req.params.query;
-	var array = [];
+app.get('/search/:query/:offset?', function(req, res) {
+	offset = req.params.offset || 1;
+	if(offset > 5){
+		res.send("This API only provides 50 results. Your offset cannot be larger than 5. Sorry!");
+	}
+	query = req.params.query;	
 	if (query === undefined) {
 		res.send({
 			"Error": "No search parameters!"
 		})
 	}
-	Bing.images(query, {
-		imageFilters: {
-			size: 'small',
-			color: 'monochrome'
-		}
-	}, function(error, res, body) {
-		db.imageSearch.create({
-			searchString: query
-		});
-		for (i = 0; i < 10; i++) {
-			var obj = {
-				imageUrl: body.d.results[i].MediaUrl,
-				pageUrl: body.d.results[i].SourceUrl,
-				altText: body.d.results[i].Title
-			};
-			array.push(obj);
-		}
-		console.log(array);
-
-	});
-	res.send("Yeah!");
-
+	var array = [];
+	async.series([
+		function() {
+			Bing.images(query, {
+				imageFilters: {
+					size: 'small',
+					color: 'monochrome'
+				}
+			}, function(error, response, body) {
+				db.imageSearch.create({
+					searchString: query
+				});
+				for (i = ((offset - 1)*10); i < offset*10; i++) {
+					var obj = {
+						imageUrl: body.d.results[i].MediaUrl,
+						pageUrl: body.d.results[i].SourceUrl,
+						altText: body.d.results[i].Title
+					};
+					array.push(obj);
+				}
+				console.log(array);
+				res.send(array);
+			});
+		}]
+	);
 });
 
 app.get('/recent', function(req, res) {
-	db.imageSearch.findAll({}).then(function(data) {
+	db.imageSearch.findAll({
+		limit: 10,
+		order: '"updatedAt" DESC'
+	}).then(function(data) {
 		res.send(data);
 	});
 });
 
-/*
-
-api parsing
-
-var array = [];
-	query = req.params.query.toString();
-	Bing.images(query, {
-    imageFilters: {
-      size: 'small',
-      color: 'monochrome'
-    }
-  }, function(error, res, body){
-	console.log(body.d.results.length); 
-	for (result of body.d.results){
-		array.push(result);
-	}
-	console.log(array);
-  });
-
-  */
-
-db.sequelize.sync({}).then(function() {
+db.sequelize.sync({force:true}).then(function() {
 	app.listen(PORT, function() {
 		console.log("Express listening on port " + PORT);
 	});
